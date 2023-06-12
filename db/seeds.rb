c36d129743ec3b93022a458c226b5a959cdf0149
timcarey <timcarey1989@gmail.com>
2023-06-12 12:23:18 -0400

    index changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 99.times do |b|
-  Blog.create!(user: User.first, content: Faker::Lorem.paragraphs(number: 5), title: Faker::Lorem.sentences(number: 4))
+  Blog.create!(user: User.first, content: Faker::Lorem.paragraphs(number: 5), title: Faker::Lorem.sentences(number: 4), category: "Tech")
 end
