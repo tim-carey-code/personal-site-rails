1755fab7763e13cb44cc6a045e39b45233a1505d
timcarey <timcarey1989@gmail.com>
2023-08-24 12:43:57 -0400

    lazy load images'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_view.image_loading = "lazy"
 end
