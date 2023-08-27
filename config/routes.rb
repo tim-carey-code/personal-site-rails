1712431df9bcc79d776505398373b08845b6be54
timcarey <timcarey1989@gmail.com>
2023-08-26 20:55:30 -0400

    redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
-Rails.application.routes.draw do
 
+Rails.application.routes.draw do
   constraints(subdomain: 'www') do
     get ':any', to: redirect(subdomain: nil, status: 301), any: /.*/
   end
+
+  # Redirect root path from www subdomain
+  get '/', to: redirect(subdomain: nil, status: 301), constraints: { subdomain: 'www' }
 
   namespace :admin do
     resources :blogs
