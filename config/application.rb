f77d0ab9071568f9d5d93466402ab65a5ef68ff5
timcarey <timcarey1989@gmail.com>
2024-01-25 08:53:15 -0500

    mini_magick active storage processor

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module PersonalSiteRails
     # config.time_zone = "Central Time (US & Canada)"
     config.time_zone = 'Eastern Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_storage.variant_processor = :mini_magick
     config.assets.css_compressor = nil
   end
 end
